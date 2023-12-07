d5fa92ed8f59204d1f03226ea3c18325553f5d7b
tylintheslayer <151826095+tylintheslayer@users.noreply.github.com>
2023-12-07 12:49:00 +0200

    firstcommit

---
+++ assignment1sln/assignment1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string milkies = Milksong.Lyrics();
+            Console.WriteLine(milkies);
+            
+        }
+    }
+}
---
+++ assignment1sln/assignment1/milksong.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    internal class Milksong
+    { 
+        public static string Lyrics() 
+        {
+            //int milk = 100;
+
+            for (int i = 99; i > 0; i--)
+            {
+                if (i >= 3)
+                {
+                    Console.WriteLine($"{i} bottle of milk on the wall, {i} bottles of milk");
+                    Console.WriteLine($"Take one down and pass it around, {i - 1} bottles of milk");
+                }
+                if (i == 1)
+                {
+                    Console.WriteLine($"{i} bottle of milk on the wall, {i} bottle of milk");
+                    break;
+                }
+                
+                if (i == 2)
+                {
+                    Console.WriteLine($"{i} bottle of milk on the wall, {i} bottles of milk");
+                    Console.WriteLine($"Take one down and pass it around, {i - 1} bottle of milk");
+                }
+                //milk--;
+            }
+            return "";
+        }
+    }
+}
